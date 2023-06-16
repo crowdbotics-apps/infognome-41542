29eea3f14fe77ec4bfb4bf64abfcf9b8678ac260
Crowdbotics <team@crowdbotics.com>
2023-06-16 18:05:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "infognome-41542",
+  url: "https://infognome-41542.botics.co",
+  api: "https://infognome-41542.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
